2de19a1455dde66e4af88700ca0ab335f3914681
Limpid136 <zjlOffice@limpiddream.com>
2025-07-16 10:13:26 +0800

    v2

--- func.go
+++ func.go
@@ -5,8 +5,8 @@ import (
 	"fmt"
 )
 
-func Func1(ctx context.Context, c1 string) string {
-	return c1
+func Func1(ctx context.Context, c1 string, c2 int) string {
+	return c1 + ":" + fmt.Sprint(c2)
 }
 func Func2(ctx context.Context, c1 int) string {
 	return fmt.Sprint(c1)
