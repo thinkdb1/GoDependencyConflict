936d682d521a2f7145119a7666580510a6bc3e6b
Limpid136 <zjlOffice@limpiddream.com>
2025-07-16 10:40:31 +0800

    v4,diff with v3 about struct

--- struct_diff/s.go
+++ struct_diff/s.go
@@ -1,16 +1,19 @@
 package struct_diff
 
+import "strconv"
+
 type S1 struct {
 	A string
+	B int
 }
 
-func (s S1) SubF1() string {
-	return s.A
+func (s S1) SubF1(c1 string) string {
+	return s.A + " :" + c1
 }
 
 func SF1(s S1) string {
-	return s.A
+	return s.A + " :" + strconv.Itoa(s.B)
 }
 func SF2(s S1) string {
-	return s.SubF1()
+	return s.SubF1("test")
 }
